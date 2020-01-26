9a3e13f6965831fa4a08a570a3e122316f335186
Trond <56018387+tronderiklarsen@users.noreply.github.com>
2020-01-26 21:12:52 +0100

    Create trond-erik.larsen-js1-ma1.js

---
+++ trond-erik.larsen-js1-ma1.js
@@ -0,0 +1,114 @@
+//1
+
+/*
+const cat = {
+    complain: function(){
+        console.log("Meow!");
+    }
+};
+*/
+
+//2
+
+// const heading = document.querySelector("h3");
+
+//3
+
+// heading.style.fontSize = "2em";
+
+//4
+
+// heading.className = "subheading";
+
+//5
+
+// const paragraph = document.querySelectorAll("p");
+
+//6
+
+// const resultsContainer = document.querySelector(".results");
+// resultsContainer.innerHTML = `<p>New paragraph</p>`;
+
+//7
+
+
+/*
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+function catArray(){
+    for (let i = 0; i < cats.length; i++) {
+        console.log(cats[i].name);
+    }
+}
+
+catArray();
+*/
+
+//8
+
+/*
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+function catArray(arg){
+    for (let i = 0; i < cats.length; i++) {
+        console.log(`<h5>${cats[i].name}</h5>`);
+    }
+}
+
+catArray();
+*/
+
+//9
+
+/*
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+function catArray(arg) {
+    for (let i = 0; i < cats.length; i++) {
+        resultsContainer.innerHTML = `<h5>${cats[i].name}</h5>`;
+    }
+}
+
+catArray();
+*/
+
+//10
+
+
+
